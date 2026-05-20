1352553eca0069bbeca36a82178a2355a18d9715
grace-kid <catamondoller@gmail.com>
2026-05-20 04:24:42 +0100

    rtyghj

--- src/component/Language.jsx
+++ src/component/Language.jsx
@@ -260,7 +260,7 @@ const Card = () => {
                 <path fillRule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clipRule="evenodd" />
               </svg>
             </span>
-            <span className="list_text md:hover:scale-110">Firebase </span>
+            <span className="list_text md:hover:scale-110">SQlite </span>
           </li>
           <h1 className="text-center button"> DEVOPS</h1>
 
